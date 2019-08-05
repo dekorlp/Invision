e3f44d4d75633741ec8f32550c18c4fc8717dfb3
Dennis Köhler <dennis.koehler92@gmx.de>
2019-08-05 05:45:59 +0200

    implemented ifdef Windows. Linux variant and max should be implemented in future

--- src/lowlevel/PlatformHardDisk.cpp
+++ src/lowlevel/PlatformHardDisk.cpp
@@ -3,17 +3,30 @@
 
 	uint32 PlatformHardDisk::CalculateTotalSpace(uint8 drive)
 	{
+#ifdef _WIN32
 		struct _diskfree_t diskfree;
 		_getdiskfree(drive, &diskfree);
 		uint32 size = (diskfree.total_clusters / 1024)*diskfree.sectors_per_cluster*diskfree.bytes_per_sector;
 		return (size / 1024);
 
+#elif __linux__
+
+#elif __APPLE__
+
+#endif
+
 	}
 	uint32 PlatformHardDisk::CalculateFreeSpace(uint8 drive)
 	{
+#ifdef _WIN32
 		struct _diskfree_t diskfree;
 		_getdiskfree(drive, &diskfree);
 		uint32 size = (diskfree.avail_clusters / 1024)*diskfree.sectors_per_cluster*diskfree.bytes_per_sector;
 		return (size / 1024);
 
+#elif __linux__
+		
+#elif __APPLE__
+
+#endif
 	}
