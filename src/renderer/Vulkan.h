d71ce723bc94d71be2c9eb5a5aa8e3b8499aace3
Dennis Köhler <dennis.koehler92@gmx.de>
2019-08-08 03:13:53 +0200

    changed headers

--- src/renderer/Vulkan.h
+++ src/renderer/Vulkan.h
@@ -1,19 +1,14 @@
-/* Copyright (C) 2019 Wildfire Games.
-* This file is part of 0 A.D.
-*
-* 0 A.D. is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* 0 A.D. is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/////////////////////////////////////////////////////////////////////////////
+// Name:	  Vulkan
+// Purpose:	  
+// Author:	  Dennis Koehler
+// Created:	  2018-01-01
+// Copyright: Copyright (c) 2014, Nano Game Foundation
+//			  This unpublished source code is proprietary to Nano Game Foundation.
+//			  The source code herein is considered trade secrets and confidential.
+//			  Reproduction or distribution, in part or complete, is prohibited except by permission of Nano Game Foundation. 
+//			  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
 
 #ifndef INCLUDED_VULKAN
 #define INCLUDED_VULKAN
