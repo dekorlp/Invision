fdb2054b96b7028c7d81649e95849f69b19ac615
Dennis Köhler <dennis.koehler92@gmx.de>
2019-11-17 17:33:50 +0100

    created VulkanPipeline Component

---
+++ src/renderer/Vulkan/VulkanPipeline.h
@@ -0,0 +1,19 @@
+#ifndef VULKAN_PIPELINE_H
+#define VULKAN_PIPELINE_H
+
+#include "vulkan\vulkan.h"
+#include "Vulkan.h"
+#include "VulkanException.h"
+
+
+namespace Invision
+{
+	class INVISION_API VulkanPipeline
+	{
+
+	};
+
+}
+
+
+#endif // VULKAN_PIPELINE_H
