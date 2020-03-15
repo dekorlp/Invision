e533ca1c6d43480c1b6760482aa9b51f22c0b7b0
Dennis Köhler <dennis.koehler92@gmx.de>
2020-03-15 17:55:37 +0100

    created VulkanDescriptorPool

---
+++ src/renderer/Vulkan/VulkanDescriptorPool.cpp
@@ -0,0 +1,11 @@
+#include "precompiled.h"
+
+
+#include "VulkanException.h"
+
+#include "VulkanDescriptorPool.h"
+
+namespace Invision
+{
+
+}
---
+++ src/renderer/Vulkan/VulkanDescriptorPool.h
@@ -0,0 +1,21 @@
+#ifndef VULKAN_DESCRIPTOR_POOL_H
+#define VULKAN_DESCRIPTOR_POOL
+
+#include "vulkan\vulkan.h"
+#include "Vulkan.h"
+
+namespace Invision
+{
+	class  VulkanDescriptorPool
+	{
+	private:
+
+
+	public:
+
+
+	};
+}
+
+
+#endif // VULKAN_DESCRIPTOR_POOL
