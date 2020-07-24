4d88fd2a65b72ad24e252556fc4a1c8f18463056
Dennis Koehler <dennis.koehler@stud.h-da.de>
2020-07-25 01:02:13 +0200

    exlude files.lua

--- build/premake/files.lua
+++ build/premake/files.lua
@@ -5,7 +5,6 @@
 Invision = {
     qt = {
         pathX64 = "D:/Programme/Qt/5.9.9/msvc2017_64",
-        pathX86 = "D:/Programme/Qt/5.9.9/msvc2015"
+        pathX86 = "D:/Programme/Qt/5.9.9/msvc2015  dsdsdsd"
     }
 }
-
