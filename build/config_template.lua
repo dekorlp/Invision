b635394368467271935b1ec4c6111123e7936b19
Dennis Köhler <denniskoehler92@gmail.com>
2020-12-08 03:39:31 +0100

    included config_template

---
+++ build/config_template.lua
@@ -0,0 +1,16 @@
+-- Configuration File
+
+--QT Configuration
+Invision = {
+    qt = {
+        pathX64 = "C:\\Qt\\5.9.9\\msvc2017_64",
+        pathX86 = "C:\\Qt\\5.9.9\\msvc2015",
+		qtModules = {"core", "gui", "widgets", "opengl"}
+    },
+	
+	inMath = {
+		matrixAlignment = "COLUMNMAJOR"    -- Options (COLUMNMAJOR | ROWMAJOR)
+	}
+}
+
+
