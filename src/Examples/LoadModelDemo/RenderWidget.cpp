4caefdb75dcd5b63df9aca6dca070ced6c0a5cd3
Dennis Köhler <denniskoehler92@gmail.com>
2021-01-29 03:00:31 +0100

    updated LoadModelDemo

--- src/Examples/LoadModelDemo/RenderWidget.cpp
+++ src/Examples/LoadModelDemo/RenderWidget.cpp
@@ -7,15 +7,15 @@
 void RenderWidget::RecreateSwapChain(const int width, const int height)
 {
 	// setup swapchain
-	graphicsInstance->ResetPresentation({ HWND(winId()), width, height });
+	graphicsInstance->ResetPresentation({ HWND(winId()), width, height }, renderPass, framebuffer, commandBuffer);
 
 	// setup framebuffer
-	framebuffer.reset();
-	framebuffer = graphicsInstance->CreateFramebuffer(renderPass, graphicsInstance->GetSizeSwapchainImages());
+	//framebuffer.reset();
+	//framebuffer = graphicsInstance->CreateFramebuffer(renderPass, graphicsInstance->GetSizeSwapchainImages());
 
 	// setup commandBuffers
-	commandBuffer.reset();
-	commandBuffer = graphicsInstance->CreateCommandBuffer(framebuffer);
+	//commandBuffer.reset();
+	//commandBuffer = graphicsInstance->CreateCommandBuffer(framebuffer);
 	BuildCommandBuffer(width, height);
 }
 
